14c9f9697d9b2c53046dd1db5a2fc91e7527d8f0
Kirlian Verus <127189354+Kyverus@users.noreply.github.com>
2024-11-13 20:41:04 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+const viteEnv = {};
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith(`VITE_`)) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key];
+  }
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  alias: {
+    "@": path.resolve(__dirname, "src"),
+  },
+  define: viteEnv,
   plugins: [react()],
-})
+});
