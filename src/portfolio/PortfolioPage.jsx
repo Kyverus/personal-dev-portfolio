4c9b7f8a70a9fb13ea676c2bbf8d821c4a1021d4
Kirlian Verus <127189354+Kyverus@users.noreply.github.com>
2025-01-24 02:05:15 +0800

    Update PortfolioPage.jsx

--- src/portfolio/PortfolioPage.jsx
+++ src/portfolio/PortfolioPage.jsx
@@ -13,7 +13,7 @@ export default function PortfolioPage() {
   return (
     <div
       style={{
-        backgroundImage: `url('${dark ? bgPatternDark : bgPatternLight}')`,
+        backgroundImage: `url("${bgPatternLight}")`,
         backgroundRepeat: "repeat",
         backgroundAttachment: "fixed",
       }}
