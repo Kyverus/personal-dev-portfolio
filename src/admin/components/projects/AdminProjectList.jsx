b53723fdf49db8e16f61713f9c4996b42d4c5732
Kirlian Verus <127189354+Kyverus@users.noreply.github.com>
2025-01-27 20:59:47 +0800

    Update AdminProjectList.jsx

--- src/admin/components/projects/AdminProjectList.jsx
+++ src/admin/components/projects/AdminProjectList.jsx
@@ -5,10 +5,13 @@ import AdminProjectItem from "./AdminProjectItem";
 export default function AdminProjectList() {
   const { projects } = useProjectContext();
   console.log(projects);
+  const sortedProjects = projects.sort(function (a, b) {
+    return b.complexity - a.complexity;
+  });
   return (
     <div className="xl:w-[1280px] mx-auto flex flex-col overflow-x-hidden space-y-10 my-5">
-      {projects &&
-        projects.map((project) => {
+      {sortedProjects &&
+        sortedProjects.map((project) => {
           return <AdminProjectItem key={project._id} project={project} />;
         })}
     </div>
